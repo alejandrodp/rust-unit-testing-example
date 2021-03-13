6a164ed9e6b9022449a75afe33226f426d00bf6d
alejandrodp <dpereiraalejandro@gmail.com>
2021-03-12 18:08:38 -0600

    Hotfix

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,26 @@
+pub fn suma(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// Esta función falla al propio, con el fin de mostrar lo que pasa
+// cuando una prueba falla. 
+fn suma_incorrectamente(a: i32, b: i32) -> i32 {
+    a - b
+}
+
 #[cfg(test)]
 mod tests {
+    // se importan las funciones
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn probar_suma() {
+            assert_eq!(suma(1, 2), 3);
+    }
+
+    #[test]
+    fn probar_suma_incorrectamente() {
+            // Claramente esta aserción va a causar un panic
+            assert_eq!(suma_incorrectamente(1, 2), 3);
     }
 }
